93a2410fc9d846558f1bdeb5b922a0e086717b33
mjpurin <mjpurin@gmail.com>
2022-02-14 13:54:19 +0900

    P349まで作成

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startPosition;
+    public float endPosition;
+
+    void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x <= endPosition) {
+            ScrollEnd();
+        }
+
+        
+    }
+    void ScrollEnd() {
+        float diff = transform.position.x - endPosition;
+        transform.position = new Vector3(startPosition + diff, 0, 0);
+        Vector3 restartPosition = transform.position;
+        restartPosition.x = startPosition + diff;
+        transform.position = restartPosition;
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+    }
+}
